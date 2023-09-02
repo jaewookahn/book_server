10f26fed68c3fb2a475ece0d32f9e3a5e3047b6e
Jae-wook Ahn <jaewook.ahn@gmail.com>
2023-09-02 03:16:15 +0000

    implement cover image and thumbnail service

--- fast.py
+++ fast.py
@@ -2,13 +2,24 @@ from typing import Union
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import StreamingResponse
 import urllib.parse
+
 from ebooklib import epub
 import ebooklib
-from starlette.responses import StreamingResponse
 import io
+from PIL import Image
 
 app = FastAPI()
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+        )
+
 
 def get_cover(file_path):
     book = epub.read_epub(file_path)
@@ -21,6 +32,13 @@ def get_cover(file_path):
             break
     return image
 
+def get_book_file_path(book_path):
+    file_root = '/data/'
+    book_path_decoded = urllib.parse.unquote_plus(book_path, encoding='utf-8', errors='replace')
+    book_path_decoded = book_path_decoded.replace('volume2', '')
+    file_path = f'{file_root}/{book_path_decoded}'
+    return file_path
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -30,15 +48,27 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/books/{book_path:path}")
-def get_book(book_path):
-    file_root = '/data/'
-    book_path_decoded = urllib.parse.unquote_plus(book_path)
-    file_path = f'{file_root}/{book_path_decoded}'
+@app.get("/books/cover/{book_path:path}")
+def get_book_cover(book_path):
+    file_path = get_book_file_path(book_path)
+    cover_image = get_cover(file_path)
+    
+    image_type = cover_image.file_name.split('.')[-1]
+    return StreamingResponse(io.BytesIO(cover_image.content), media_type=f"image/{image_type}")
+
+@app.get('/books/thumbnail/{book_path:path}')
+def get_book_cover_thumbnail(book_path):
+    file_path = get_book_file_path(book_path)
     cover_image = get_cover(file_path)
 
-    return StreamingResponse(io.BytesIO(cover_image.content), media_type="image/jpg")
+    pimg = Image.open(io.BytesIO(cover_image.content))
+    pimg.thumbnail((100,100))
+    with io.BytesIO() as output:
+        pimg.save(output, format='png')
+        pcontents = output.getvalue()
 
+    return StreamingResponse(io.BytesIO(pcontents), media_type='image/png')
+ 
 @app.get("/file/{path}")
 async def get_file(path):
     some_file_path = f'/data/{path}'
